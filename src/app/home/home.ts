a1d90a68ad5ee7dc4d20ff0ef627691ed63f1cac
ichsan182 <89898342+ichsan182@users.noreply.github.com>
2026-02-25 22:51:38 +0700

    home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Sidebar } from '../shared/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Sidebar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
 }
---
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+  protected profileName = 'John Doe';
+  protected profileEmail = 'john@example.com';
+  protected profileImage = 'assets/profile-avatar.png';
+
+  protected navItems = [
+    { path: '/home', label: 'Home', icon: 'assets/material-symbols_home.svg' },
+    {
+      path: '/transactions',
+      label: 'Transaction',
+      icon: 'assets/material-symbols_transaction.svg',
+    },
+    {
+      path: '/investment',
+      label: 'Investment',
+      icon: 'assets/material-symbols_investation.svg',
+    },
+    {
+      path: '/tools',
+      label: 'Tools',
+      icon: 'assets/material-symbols_tools.svg',
+    },
+    {
+      path: '/education',
+      label: 'Education',
+      icon: 'assets/material-symbols_education.svg',
+    },
+    {
+      path: '/chat',
+      label: 'Chat',
+      icon: 'assets/material-symbols_education.svg',
+    },
+  ];
+}
