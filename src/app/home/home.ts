5048a052de0870340d1c728e06b8fc0fac58b308
ichsan182 <89898342+ichsan182@users.noreply.github.com>
2026-03-12 21:32:02 +0700

    level pro max update home + rename

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -22,6 +22,41 @@ export class Home {
   pengeluaranPercentage = this.generatePercentage();
   hutangPercentage = this.generatePercentage();
 
+  currentLevel = 2;
+  levelProgress = 60;
+
+  readonly levelImages = [
+    'assets/level/level_1.svg',
+    'assets/level/level_2.svg',
+    'assets/level/level_3.svg',
+    'assets/level/level_4.svg',
+    'assets/level/level_5.svg',
+    'assets/level/level-6.svg',
+    'assets/level/level_7.svg',
+  ];
+
+  readonly levelTasks = [
+    'Mulai mencatat pengeluaran harianmu',
+    'Catat transaksi selama 7 hari berturut-turut',
+    'Mengosongkan hutang',
+    'Capai tabungan darurat 3x pengeluaran',
+    'Mulai investasi pertama kamu',
+    'Raih streak 100 hari berturut-turut',
+    'Capai semua target keuangan',
+  ];
+
+  get currentLevelImage(): string {
+    return this.levelImages[this.currentLevel - 1];
+  }
+
+  get nextLevelImage(): string | null {
+    return this.currentLevel < 7 ? this.levelImages[this.currentLevel] : null;
+  }
+
+  get currentLevelTask(): string {
+    return this.levelTasks[this.currentLevel - 1];
+  }
+
   private generatePercentage(): number {
     return Math.floor(Math.random() * 100) + 1;
   }
